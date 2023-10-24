6a0eec350d6558e7772dbde59e1f88f71201c300
papachio <Ioannis.Papachristos@arhs-dev-hellas.com>
2023-10-24 10:42:14 +0300

    Creation of the Panel of the snake game

--- src/com/snakegame/GameFrame.java
+++ src/com/snakegame/GameFrame.java
@@ -13,6 +13,9 @@ public class GameFrame extends JFrame {
         this.setTitle(TITLE);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 
 }
